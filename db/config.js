e74422ce9167c3a3525bbddd4eb425b8981773fd
higiniecito <cdramirezh@unal.edu.co>
2024-03-19 11:08:46 -0500

    Set up para migrations

---
+++ db/config.js
@@ -0,0 +1,17 @@
+const { config } = require('../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const ENGINE = config.dbEngine;
+const URI = `${ENGINE}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+module.exports = {
+  development: {
+    url: URI,
+    dialect: ENGINE,
+  },
+  production: {
+    url: URI,
+    dialect: ENGINE,
+  },
+};
