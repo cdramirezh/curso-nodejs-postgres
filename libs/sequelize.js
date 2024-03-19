b3c54575e2fd7581a8d969e79eb7d90bb3c7198b
higiniecito <cdramirezh@unal.edu.co>
2024-03-19 10:04:03 -0500

    Added db engine to environment vars

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -6,10 +6,11 @@ const { config } = require('../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const ENGINE = config.dbEngine;
+const URI = `${ENGINE}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
+  dialect: ENGINE,
   logging: (vaina) => console.log(chalk.bgCyan(vaina)),
 });
 
