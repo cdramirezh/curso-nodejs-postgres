107922645c65157ddb8e735bbfd7205a3f9de2f2
higiniecito <cdramirezh@unal.edu.co>
2024-03-24 11:18:27 -0500

    Dos migraciones que no salieron como yo quería

---
+++ db/migrations/20240324160931-chage-customer-fk-to-user.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+const { CUSTOMER_TABLE } = require('../models/customer.model');
+const { USER_TABLE } = require('../models/user.model');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.changeColumn(CUSTOMER_TABLE, 'user_id', {
+      name: 'user_id',
+      allowNull: true,
+      type: DataTypes.INTEGER,
+      references: {
+        model: USER_TABLE,
+        key: 'id',
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    //.await queryInterface.dropTable('users');
+  },
+};
