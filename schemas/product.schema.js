305d52020a6abb6f510ca47a53e1151020cfc43c
higiniecito <cdramirezh@unal.edu.co>
2024-04-03 10:36:02 -0500

    Min y max

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -9,6 +9,8 @@ const description = Joi.string().min(10);
 const isBlocked = Joi.boolean();
 const limit = Joi.number().integer().min(0);
 const offset = Joi.number().integer().min(0);
+const price_min = Joi.number().integer();
+const price_max = Joi.number().integer();
 
 const createProductSchema = Joi.object({
   name: name.required(),
@@ -31,9 +33,15 @@ const getProductSchema = Joi.object({
   id: id.required(),
 });
 
-const queryProductSchema = Joi.object()
-  .keys({ limit, offset })
-  .and('limit', 'offset');
+const queryProductSchema = Joi.object({
+  limit,
+  offset,
+  price,
+  price_min,
+  price_max: price_max.greater(Joi.ref('price_min')),
+})
+  .and('limit', 'offset')
+  .without('price', ['price_min', 'price_max']);
 
 module.exports = {
   createProductSchema,
--- services/product.service.js
+++ services/product.service.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const { models } = require('../libs/sequelize');
 const boom = require('@hapi/boom');
 
@@ -8,11 +9,19 @@ class ProductsService {
   }
 
   async find(query) {
-    let options = {};
-    const { limit, offset } = query;
+    // build the options object regarding query
+    let options = {
+      where: {},
+    };
+    const { limit, offset, price, price_min, price_max } = query;
     if (limit !== undefined && offset !== undefined) {
       options.limit = limit;
       options.offset = offset;
+    }
+    if (price !== undefined) options.where.price = price;
+    else options.where.price = {
+      ...((price_min !== undefined) && {[Op.gte]: price_min}),
+      ...((price_max !== undefined) && {[Op.lte]: price_max}),
     }
 
     const data = await models.Product.findAll(options);
@@ -20,7 +29,9 @@ class ProductsService {
   }
 
   async findOne(id) {
-    const product = await models.Product.findByPk(id, { include: ['category'] });
+    const product = await models.Product.findByPk(id, {
+      include: ['category'],
+    });
     if (!product) throw boom.notFound('product not found');
     if (product.isBlocked) throw boom.conflict('product is blocked');
     return product;
