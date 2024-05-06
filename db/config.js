11c24371892f7c55bbe2cfa8842ef38f38564f8c
higiniecito <cdramirezh@unal.edu.co>
2024-05-06 13:13:53 -0500

    Change some more stuff in hope migrations will run

--- db/config.js
+++ db/config.js
@@ -3,7 +3,12 @@ const { config } = require('../src/config/config');
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const ENGINE = config.dbEngine;
-const URI = `${ENGINE}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+let URI;
+if (config.dbPrivateUrl) URI = config.dbPrivateUrl;
+else if (config.dbUrl) URI = config.dbUrl;
+else
+  URI = `${ENGINE}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
   development: {
